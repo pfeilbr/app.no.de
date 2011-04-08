50222b19083060626637bcc8e1cd6827d5341984
Brian Pfeil <brian.pfeil@gmail.com>
2011-04-08 12:17:38 -0400

    hello world commit 8

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ app.configure(function(){
     app.use(app.router);
 });
 
+var users = [
+    { id: 0, name: 'tj', email: 'tj@vision-media.ca', role: 'member' }
+  , { id: 1, name: 'ciaran', email: 'ciaranj@gmail.com', role: 'member' }
+  , { id: 2, name: 'aaron', email: 'aaron.heckmann+github@gmail.com', role: 'admin' }
+];
+
 app.get('/', function(req, res){
-    res.send('Hello World from Express part 2');
+    res.send('Hello World from Express part 2<br/>' + users);
 });
 
 app.listen(process.env.PORT || 8001);
