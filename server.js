18033f4d451924c418ca53904989901de13eedea
Brian Pfeil <brian.pfeil@gmail.com>
2011-04-08 13:18:01 -0400

    hello world commit 9

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var _ = require('underscore');
 var express = require('express');
 var app = express.createServer();
-
+console.log(express.static);
 app.configure(function(){
     app.use(express.static(__dirname + '/public'));    
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(app.router);
 });
-
-var users = [
-    { id: 0, name: 'tj', email: 'tj@vision-media.ca', role: 'member' }
-  , { id: 1, name: 'ciaran', email: 'ciaranj@gmail.com', role: 'member' }
-  , { id: 2, name: 'aaron', email: 'aaron.heckmann+github@gmail.com', role: 'admin' }
-];
 
 app.get('/', function(req, res){
     res.send('underscore exists = ' + _);
