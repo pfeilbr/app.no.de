e405cfe94d2c482ce7fb6f4b190b0f979a7ad259
Brian Pfeil <brian.pfeil@gmail.com>
2011-04-07 22:24:12 -0400

    hello world commit 7

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer();
+
+app.configure(function(){
+    app.use(express.static(__dirname + '/public'));    
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
 
 app.get('/', function(req, res){
     res.send('Hello World from Express');
